b1b27dd7d90d3066a36b3254d5be1e2a9f9acb83
kevinhsu7361 <applewish9@gmail.com>
2020-08-25 14:32:15 +0800

    carousel

--- carousel.js
+++ carousel.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         });
         tabCarousel(special_owl, '.world-catagory-slider', '往左移動', '往右移動')
     
-        let owl = $('.slides').owlCarousel({
+        let owl = $('.second-carousel').owlCarousel({
         items: 4,
         margin: 0,
         loop: true,
@@ -21,5 +21,5 @@ $(document).ready(function () {
         smartSpeed: 1000,
         nav: true
     });
-    tabCarousel(owl, '.slides', '往左移動', '往右移動')
+    tabCarousel(owl, '.second-carousel', '往左移動', '往右移動')
 });
